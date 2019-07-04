57a0212a8fad5a422bb9caa80184093bed84d91d
qinchuan <317423553@qq.com>
2019-07-04 22:19:04 +0800

    添加一张图片

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
  */
 import $ from 'jquery'
 import './css/index.css'
+import './css/index.scss'
+import 'bootstrap/dist/css/bootstrap.css'
 
 $(function(){
-    $('li:odd').css("backgroundColor","lightblue")
+    $('li:odd').css("backgroundColor","pink")
     $('li:even').css("backgroundColor",function(){
         return '#' + 'abcdef'
     })
