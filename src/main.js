cf35e4e94766571ed268b9f59f72ccac476ec49e
qinchuan2019 <317423553@qq.com>
2019-07-04 18:14:52 +0800

    dddd

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@
  * 
  */
 import $ from 'jquery'
+import './css/index.css'
 
 $(function(){
-    $('li:odd').css("backgroundColor","yellow")
+    $('li:odd').css("backgroundColor","lightblue")
     $('li:even').css("backgroundColor",function(){
         return '#' + 'abcdef'
     })
